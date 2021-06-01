6ea40a7b42dd0c62a04592a927016077bb54d0a8
bherbruck <brennen.herbruck@gmail.com>
2021-06-01 16:29:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -P ./
-unzip ./ngrok-stable-linux-amd64.zip
-rm ./ngrok-stable-linux-amd64.zip
+unzip ./ngrok-stable-linux-arm.zip
+rm ./ngrok-stable-linux-arm.zip
 
 mkdir /home/$USER/.ngrok2
 cp ./ngrok.yml /home/$USER/.ngrok2/ngrok.yml
