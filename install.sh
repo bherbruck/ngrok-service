344095787cc3e5c6177ed73ac2f98dfa5042445c
bherbruck <brennen.herbruck@gmail.com>
2021-06-01 16:26:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -P ./
 unzip ./ngrok-stable-linux-amd64.zip
 rm ./ngrok-stable-linux-amd64.zip
+
+mkdir $USER/.ngrok2
+cp ./ngrok.yml $USER/.ngrok2/ngrok.yml
+
 sudo mv ./ngrok /usr/local/bin/
-mv ./ngrok.yml $USER/.ngrok2/ngrok.yml
 sudo cp ./ngrok.service /etc/systemd/system/ngrok.service
+
 sudo systemctl start ngrok
 sudo systemctl enable ngrok
 sudo systemctl status ngrok
