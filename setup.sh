e7bf01714efb494469c6c684c5ea2f3c7f00bc29
bherbruck <brennen.herbruck@gmail.com>
2022-09-16 20:54:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,21 +12,48 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -P ./
-unzip ./ngrok-stable-linux-arm.zip
-rm ./ngrok-stable-linux-arm.zip
 
-mv ./ngrok /usr/local/bin/
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+DOWNLOAD_PATH=/tmp/ngrok.tgz
+DOWNLOAD_FILE=""
+
+case $ARCH in
+  "x86_64")
+    DOWNLOAD_FILE="ngrok-v3-stable-linux-amd64.tgz"
+    ;;
+  "i386")
+    DOWNLOAD_FILE="ngrok-v3-stable-linux-386.tgz"
+    ;;
+  "armv7l")
+    DOWNLOAD_FILE="ngrok-v3-stable-linux-arm.tgz"
+    ;;
+  "aarch64")
+    DOWNLOAD_FILE="ngrok-v3-stable-linux-arm64.tgz"
+    ;;
+  *)
+    echo "unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+curl -L https://bin.equinox.io/c/bNyj1mQVY4c/$DOWNLOAD_FILE -o $DOWNLOAD_PATH --compressed
+tar -xvzf $DOWNLOAD_PATH -C /tmp
+mv /tmp/ngrok /usr/local/bin/ngrok
 
 mkdir -p /home/$SUDO_USER/.ngrok2/
 cat > /home/$SUDO_USER/.ngrok2/ngrok.yml << EOF
 authtoken: $1
-
+version: '2'
 tunnels:
   ssh:
     proto: tcp
     addr: 22
 EOF
+
+chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.ngrok2
+
 
 cat > /etc/systemd/system/ngrok.service << EOF
 [Service]
@@ -36,10 +63,10 @@ WorkingDirectory=/home/$SUDO_USER
 ExecStart=/usr/local/bin/ngrok start --all
 Restart=always
 RestartSec=10
-
 [Install]
 WantedBy=multi-user.target
 EOF
+
 
 systemctl daemon-reload
 systemctl start ngrok
